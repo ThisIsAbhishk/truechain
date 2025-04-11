64b6d7b0601c79a8a3b8718b826c4d90a5051c18
ThisIsAbhishk <131981975+ThisIsAbhishk@users.noreply.github.com>
2025-04-11 14:14:41 +0530

    Update Product.jsx

--- truechain-frontend-react/src/components/pages/Product.jsx
+++ truechain-frontend-react/src/components/pages/Product.jsx
@@ -97,7 +97,7 @@ const Product = () => {
     const getImage = async (imageName) => {
         setImage(prevState => ({
             ...prevState,
-            filepreview: `http://localhost:5000/file/product/${imageName}`
+            filepreview: `https://truechain.onrender.com/file/product/${imageName}`
             })
         )
     }
